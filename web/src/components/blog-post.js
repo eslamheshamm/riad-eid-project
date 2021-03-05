20f1b2dbf93a251e8828587dd2c8b29a4b72a10b
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-05 20:36:45 +0200

    adjust styles and ramove unnecessery info

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,42 +1,42 @@
-import {format, distanceInWords, differenceInDays} from 'date-fns'
-import React from 'react'
-import {buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
-import PortableText from './portableText'
-import Container from './container'
-import AuthorList from './author-list'
+import { format, distanceInWords, differenceInDays } from "date-fns";
+import React from "react";
+import { buildImageObj } from "../lib/helpers";
+import { imageUrlFor } from "../lib/image-url";
+import PortableText from "./portableText";
+import Container from "./container";
+import AuthorList from "./author-list";
 
-import styles from './blog-post.module.css'
+import styles from "./blog-post.module.css";
 
-function BlogPost (props) {
-  const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
+function BlogPost(props) {
+  const { _rawBody, authors, categories, title, mainImage, publishedAt } = props;
   return (
-    <article className={styles.root}>
+    <article className="w-7/12 mx-auto">
       {mainImage && mainImage.asset && (
         <div className={styles.mainImage}>
           <img
             src={imageUrlFor(buildImageObj(mainImage))
               .width(1200)
               .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .auto('format')
+              .fit("crop")
+              .auto("format")
               .url()}
             alt={mainImage.alt}
           />
         </div>
       )}
       <Container>
-        <div className={styles.grid}>
+        <div>
           <div className={styles.mainContent}>
-            <h1 className={styles.title}>{title}</h1>
-            {_rawBody && <PortableText blocks={_rawBody} />}
+            <h1 className={`font-yasser text-4xl my-6`}>{title}</h1>
+            <div className="mb-32"> {_rawBody && <PortableText blocks={_rawBody} />}</div>
           </div>
-          <aside className={styles.metaContent}>
+          {/* <aside className={styles.metaContent}>
             {publishedAt && (
               <div className={styles.publishedAt}>
                 {differenceInDays(new Date(publishedAt), new Date()) > 3
                   ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), 'MMMM Do, YYYY')}
+                  : format(new Date(publishedAt), "MMMM Do, YYYY")}
               </div>
             )}
             {authors && <AuthorList items={authors} title='Authors' />}
@@ -50,11 +50,11 @@ function BlogPost (props) {
                 </ul>
               </div>
             )}
-          </aside>
+          </aside> */}
         </div>
       </Container>
     </article>
-  )
+  );
 }
 
-export default BlogPost
+export default BlogPost;
