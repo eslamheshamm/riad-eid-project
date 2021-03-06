200a39bf305e9fe4c772e63d08b2b0d0b373de7e
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-06 23:06:29 +0200

    comment unused functions

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,15 +1,15 @@
-import { format, distanceInWords, differenceInDays } from "date-fns";
+// import { format, distanceInWords, differenceInDays } from "date-fns";
 import React from "react";
 import { buildImageObj } from "../lib/helpers";
 import { imageUrlFor } from "../lib/image-url";
 import PortableText from "./portableText";
 import Container from "./container";
-import AuthorList from "./author-list";
-
+// import AuthorList from "./author-list";
+// authors, categories,publishedAt
 import styles from "./blog-post.module.css";
 
 function BlogPost(props) {
-  const { _rawBody, authors, categories, title, mainImage, publishedAt } = props;
+  const { _rawBody, title, mainImage } = props;
   return (
     <article className="w-7/12 mx-auto">
       {mainImage && mainImage.asset && (
