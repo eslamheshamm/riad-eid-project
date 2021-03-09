95a5ef34e8fe8c7ff9159d9418637f000dd46679
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-09 08:09:20 +0200

    adjust video size

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Figure from "./Figure";
-import MainImage from "./MainImage";
+// import Figure from "./Figure";
+import MainImage from "./blog/MainImage";
 import ReactPlayer from "react-player";
 import InstagramEmbed from "react-instagram-embed";
 import LatexRenderer from "./Latex";
-
+import styles from "./serializers.module.css";
 const AuthorReference = ({ node }) => {
   if (node && node.author && node.author.name) {
     return <span>{node.author.name}</span>;
@@ -16,13 +16,23 @@ const serializers = {
   types: {
     authorReference: AuthorReference,
     mainImage: ({ node }) => <MainImage mainImage={node} />,
-    videoEmbed: ({ node }) => <ReactPlayer className="mt-6 mb-6" url={node.url} controls />,
+    videoEmbed: ({ node }) => (
+      <div className={styles.playerWrapper}>
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          className={styles.reactPlayer}
+          url={node.url}
+          controls
+        />
+      </div>
+    ),
     instagram: ({ node }) => {
       if (!node.url) return null;
       return <InstagramEmbed url={node.url} className="container mx-auto mt-6 mb-6" />;
     },
-    math: ({ node, isInline = false }) => <LatexRenderer isInline={isInline} latex={node.latex} />,
-  },
+    math: ({ node, isInline = false }) => <LatexRenderer isInline={isInline} latex={node.latex} />
+  }
 };
 
 export default serializers;
