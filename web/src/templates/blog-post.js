b47c0dc2e9d68014be8399d8c04ce45773c6505f
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-09 08:11:15 +0200

    ramoving unnecessery queries, fix import

--- web/src/templates/blog-post.js
+++ web/src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import Container from "../components/container";
 import GraphQLErrorList from "../components/graphql-error-list";
-import BlogPost from "../components/blog-post";
+import BlogPost from "../components/blog/blog-post";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import { toPlainText } from "../lib/helpers";
@@ -12,10 +12,6 @@ export const query = graphql`
     post: sanityPost(id: { eq: $id }) {
       id
       publishedAt
-      categories {
-        _id
-        title
-      }
       mainImage {
         ...SanityImage
         alt
@@ -26,38 +22,11 @@ export const query = graphql`
       }
       _rawExcerpt(resolveReferences: { maxDepth: 5 })
       _rawBody(resolveReferences: { maxDepth: 5 })
-      authors {
-        _key
-        author {
-          image {
-            crop {
-              _key
-              _type
-              top
-              bottom
-              left
-              right
-            }
-            hotspot {
-              _key
-              _type
-              x
-              y
-              height
-              width
-            }
-            asset {
-              _id
-            }
-          }
-          name
-        }
-      }
     }
   }
 `;
 
-const BlogPostTemplate = (props) => {
+const BlogPostTemplate = props => {
   const { data, errors } = props;
   const post = data && data.post;
   return (
