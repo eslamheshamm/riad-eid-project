e47ab885f98cc82e04980e835725aa2bd93d2efa
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-11 10:28:47 +0200

    creating pages

--- web/src/pages/questions.js
+++ web/src/pages/questions.js
@@ -1,89 +1,79 @@
 import React from "react";
-// import { graphql } from "gatsby";
-// import {
-//   mapEdgesToNodes,
-//   filterOutDocsWithoutSlugs,
-//   filterOutDocsPublishedInTheFuture
-// } from "../lib/helpers";
-// import BlogPostPreviewList from "../components/blog-post-preview-list";
-// import Container from "../components/container";
-// import GraphQLErrorList from "../components/graphql-error-list";
-// import SEO from "../components/seo";
 import Layout from "../containers/layout";
 
-// export const query = graphql`
-//   query IndexPageQuery {
-//     site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
-//       title
-//     }
-//     posts: allSanityPost(
-//       limit: 6
-//       sort: { fields: [publishedAt], order: DESC }
-//       filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
-//     ) {
-//       edges {
-//         node {
-//           id
-//           publishedAt
-//           mainImage {
-//             ...SanityImage
-//             alt
-//           }
-//           title
-//           _rawExcerpt
-//           slug {
-//             current
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+import { graphql } from "gatsby";
+import { mapEdgesToNodes, filterOutDocsPublishedInTheFuture } from "../lib/helpers";
+import QoustionsPostPreviewList from "../components/qoustions/qoustions-post-preview-list";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import SEO from "../components/seo";
 
-const Questions = props => {
-  //   const { data, errors } = props;
+export const query = graphql`
+  query AllQuestionsPosts {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+    }
+    posts: allSanityQuestions(
+      limit: 6
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { publishedAt: { ne: null } }
+    ) {
+      edges {
+        node {
+          id
+          publishedAt
+          title
+          _rawExcerpt
+        }
+      }
+    }
+  }
+`;
+const Qoustions = ({ data, errors }) => {
+  // return (
+  //   <Layout>
+  //     <h1>Pres Page</h1>
+  //   </Layout>
+  // );
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
 
-  //   if (errors) {
-  //     return (
-  //       <Layout>
-  //         <GraphQLErrorList errors={errors} />
-  //       </Layout>
-  //     );
-  //   }
-
-  //   const site = (data || {}).site;
-  //   const postNodes = (data || {}).posts
-  //     ? mapEdgesToNodes(data.posts)
-  //         .filter(filterOutDocsWithoutSlugs)
-  //         .filter(filterOutDocsPublishedInTheFuture)
-  //     : [];
-
-  //   if (!site) {
-  //     console.warn(
-  //       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-  //     );
-  //   }
+  const site = (data || {}).site;
+  const postNodes = (data || {}).posts
+    ? mapEdgesToNodes(data.posts).filter(filterOutDocsPublishedInTheFuture)
+    : [];
+  console.log(postNodes);
+  if (!site) {
+    console.warn(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    );
+  }
 
   return (
     <Layout>
-      {/* <SEO
+      <SEO
         title={site.title || "Missing title"}
         description={site.description || "Missing description"}
         keywords={site.keywords || []}
       />
       <Container>
-        <div className="flex flex-col mb-12 ">
-          <h1 className="mb-4 text-4xl font-yasser">مقالات عن صحة الأطفال</h1>
+        <div className="flex flex-col items-center mb-12 ">
+          <h1 className="mb-4 text-4xl font-yasser">توصيات</h1>
           <p>
             نسبة النجاح في هذا الامر تصل إلي 95% في حال التزام الزوجين بجميع التوصيات المذكورة في
             البرنامج.
           </p>
         </div>
-        <div className="py-6 mb-56">{postNodes && <BlogPostPreviewList nodes={postNodes} />}</div>
-      </Container> */}
-      <h1>This is Q&A page</h1>
+        <div className="py-6 mb-56">
+          {postNodes && <QoustionsPostPreviewList nodes={postNodes} />}
+        </div>
+      </Container>
     </Layout>
   );
 };
-
-export default Questions;
+export default Qoustions;
--- web/src/pages/reviews.js
+++ web/src/pages/reviews.js
@@ -1,10 +1,75 @@
 import React from "react";
 import Layout from "../containers/layout";
 
-const Reviews = () => {
+import { graphql } from "gatsby";
+import { mapEdgesToNodes, filterOutDocsPublishedInTheFuture } from "../lib/helpers";
+import ReviewsPostPreviewList from "../components/reviews/reviews-post-preview-list";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import SEO from "../components/seo";
+
+export const query = graphql`
+  query AllReviewsPosts {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+    }
+    posts: allSanityReviews(
+      limit: 6
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { publishedAt: { ne: null } }
+    ) {
+      edges {
+        node {
+          id
+          publishedAt
+          mainImage {
+            ...SanityImage
+            alt
+          }
+          title
+          _rawExcerpt
+        }
+      }
+    }
+  }
+`;
+const Reviews = ({ data, errors }) => {
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
+
+  const site = (data || {}).site;
+  const postNodes = (data || {}).posts ? mapEdgesToNodes(data.posts) : [];
+  console.log(postNodes);
+  if (!site) {
+    console.warn(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    );
+  }
+
   return (
     <Layout>
-      <h1>Reviews Page</h1>
+      <SEO
+        title={site.title || "Missing title"}
+        description={site.description || "Missing description"}
+        keywords={site.keywords || []}
+      />
+      <Container>
+        <div className="flex flex-col items-center mb-12 ">
+          <h1 className="mb-4 text-4xl font-yasser">توصيات</h1>
+          <p>
+            نسبة النجاح في هذا الامر تصل إلي 95% في حال التزام الزوجين بجميع التوصيات المذكورة في
+            البرنامج.
+          </p>
+        </div>
+        <div className="py-6 mb-56">
+          {postNodes && <ReviewsPostPreviewList nodes={postNodes} />}
+        </div>
+      </Container>
     </Layout>
   );
 };
