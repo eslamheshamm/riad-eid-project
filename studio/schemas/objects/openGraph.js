ab5b857de76dbd59c313e1bde0236146c38ebe42
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-13 23:24:26 +0200

    ramoved comment

--- studio/schemas/objects/openGraph.js
+++ studio/schemas/objects/openGraph.js
@@ -8,40 +8,32 @@ export default {
       name: 'title',
       type: 'string',
       description: 'Heads up! This will override the page title.',
-      validation: Rule => Rule.max(60).warning('Should be under 60 characters')
+      validation: (Rule) => Rule.max(60).warning('Should be under 60 characters'),
     },
     {
       title: 'Description',
       name: 'description',
       type: 'text',
-      validation: Rule => Rule.max(155).warning('Should be under 155 characters')
+      validation: (Rule) => Rule.max(155).warning('Should be under 155 characters'),
     },
     {
       title: 'Image',
       description: 'Facebook recommends 1200x630 (will be auto resized)',
       name: 'image',
-      type: 'mainImage'
-    }
-    /*
-    // You can add videos to Open Graph tags too
-    {
-      name: 'video',
-      title: 'Video',
-      type: 'mux.video'
-    }
-    */
+      type: 'mainImage',
+    },
   ],
   preview: {
     select: {
       title: 'title',
       route: 'route.slug.current',
-      link: 'link'
+      link: 'link',
     },
     prepare({ title, route, link }) {
       return {
         title,
-        subtitle: route ? `Route: /${route}/` : link ? `External link: ${link}` : 'Not set'
+        subtitle: route ? `Route: /${route}/` : link ? `External link: ${link}` : 'Not set',
       }
-    }
-  }
+    },
+  },
 }
