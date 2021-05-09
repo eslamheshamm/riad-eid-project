f3bf27d1513be1c674a16cd7b7a0a8e44c946fe6
eslamheshamm <esl2mhesham@gmail.com>
2021-05-10 00:49:13 +0200

    ramoving comments

--- web/src/components/blog/blog-post.js
+++ web/src/components/blog/blog-post.js
@@ -4,8 +4,7 @@ import { buildImageObj } from "../../lib/helpers";
 import { imageUrlFor } from "../../lib/image-url";
 import PortableText from "../portableText";
 import Container from "../container";
-// import AuthorList from "./author-list";
-// authors, categories,publishedAt
+
 import styles from "./blog-post.module.css";
 
 function BlogPost(props) {
@@ -31,26 +30,6 @@ function BlogPost(props) {
             <h1 className={`font-yasser text-4xl my-6`}>{title}</h1>
             <div className="mb-32"> {_rawBody && <PortableText blocks={_rawBody} />}</div>
           </div>
-          {/* <aside className={styles.metaContent}>
-            {publishedAt && (
-              <div className={styles.publishedAt}>
-                {differenceInDays(new Date(publishedAt), new Date()) > 3
-                  ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), "MMMM Do, YYYY")}
-              </div>
-            )}
-            {authors && <AuthorList items={authors} title='Authors' />}
-            {categories && (
-              <div className={styles.categories}>
-                <h3 className={styles.categoriesHeadline}>Categories</h3>
-                <ul>
-                  {categories.map(category => (
-                    <li key={category._id}>{category.title}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </aside> */}
         </div>
       </Container>
     </article>
