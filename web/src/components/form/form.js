23f0affc64416b5e0cdb867cbff4a4efe12d5acc
eslamheshamm <esl2mhesham@gmail.com>
2021-05-10 04:07:05 +0200

    form

--- web/src/components/form/form.js
+++ web/src/components/form/form.js
@@ -1,38 +1,39 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
 const FormInputs = () => {
-  // function encode(data) {
-  //   return Object.keys(data)
-  //     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  //     .join("&");
-  // }
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm();
-  // const [succes, setSucces] = React.useState(false);
-  // const [disable, setDisable] = React.useState(false);
-  // const onSubmit = (data, e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": form.getAttribute("name"),
-  //       ...data
-  //     })
-  //   })
-  //     .then(res => {
-  //       console.log(res);
-  //       setSucces(true);
-  //       setDisable(true);
-  //     })
-  //     .catch(error => alert(error));
-  // };
-  const onSubmit = data => console.log(data);
+  const [succes, setSucces] = React.useState(false);
+  const [disable, setDisable] = React.useState(false);
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...data
+      })
+    })
+      .then(res => {
+        console.log(res);
+        setSucces(true);
+        setDisable(true);
+      })
+      .catch(error => alert(error));
+  };
+  // const onSubmit = data => console.log(data);
 
   return (
     // <form
@@ -44,43 +45,7 @@ const FormInputs = () => {
     //   data-netlify-honeypot="bot-field"
     //   netlify
     // >
-    //   <input type="hidden" name="form-name" value="contact" />
-    //   <p class="hidden">
-    //     <label>
-    //       Don’t fill this out if you’re human: <input name="bot-field" />
-    //     </label>
-    //   </p>
-    //   <label className="mb-5">
-    //     <input
-    //       name="question"
-    //       type="text"
-    //       placeholder="السؤال"
-    //       ref={register({ required: true })}
-    //       className="p-8 border rounded-2xl focus:ring-1 focus:ring-primary focus:outline-none block w-full"
-    //     />
-    //     {errors.email && <span>This field is required</span>}
-    //   </label>
-    // <label className="mb-5 w-full">
-    //   <input
-    //     name="name"
-    //     type="text"
-    //     placeholder="Your name / company’s name"
-    //     className="p-8 border rounded-2xl focus:ring-1 focus:ring-primary focus:outline-none block w-full"
-    //     ref={register({ required: true })}
-    //   />
-    //   {errors.name && <span>This field is required</span>}
-    // </label>
-    //   <label className="w-full mb-8">
-    //     <textarea
-    //       placeholder="Your message"
-    //       name="message"
-    //       ref={register({ required: true, minLength: 15, maxLength: 150 })}
-    //       className="resize-none border rounded-2xl h-72 p-8  focus:ring-1 focus:ring-primary focus:outline-none w-full block"
-    //     />
-    //     {errors.message?.type === "required" && <span>This field is required</span>}
-    //     {errors.message?.type === "minLength" && <span>required to be more than 15 character</span>}
-    //     {errors.message?.type === "maxLength" && <span>Should be less than 150 character </span>}
-    //   </label>
+
     //   {succes && <span>Thank you!</span>}
 
     //   <button
@@ -106,8 +71,17 @@ const FormInputs = () => {
         className="text-black flex flex-col font-yasser"
         autoComplete="off"
         name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
         netlify
       >
+        <input type="hidden" name="form-name" value="contact" />
+        <p class="hidden">
+          <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+          </label>
+        </p>
         <label className="mb-5 w-full">
           <input
             name="question"
@@ -126,12 +100,8 @@ const FormInputs = () => {
             {...register("phoneNumber", {
               required: "This field is required",
               pattern: {
-                value: /[0-9]/,
+                value: /[0-9]{9}/,
                 message: "Only numbers"
-              },
-              minLength: {
-                value: 9,
-                message: "complete your phone number" // JS only: <p>error message</p> TS only support string
               }
             })}
             className="py-8 px-6 border rounded-2xl focus:ring-1 focus:ring-primary-DEFAULT focus:outline-none block w-full"
@@ -146,9 +116,16 @@ const FormInputs = () => {
             className="resize-none border rounded-2xl h-48 py-8 px-6  focus:ring-1 focus:ring-primary-DEFAULT focus:outline-none w-full block"
           />
         </label>
+        {succes && <span>شكراً.</span>}
+
         <button
           type="submit"
-          className="self-start p-6   rounded-2xl  bg-primary-DEFAULT font-semibold text-black text-lg"
+          className={
+            disable
+              ? "self-start p-6   rounded-2xl  bg-primary-DEFAULT font-semibold text-black text-lg opacity-50 cursor-auto"
+              : "self-start p-6   rounded-2xl  bg-primary-DEFAULT font-semibold text-black text-lg"
+          }
+          disabled={disable}
         >
           الإرسال الأن
         </button>
