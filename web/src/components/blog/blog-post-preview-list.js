b8445089c4f41a8483142facbd0fed9e301fe3b2
eslamheshamm <esl2mhesham@gmail.com>
2021-05-10 16:20:31 +0200

    adjusting grid

--- web/src/components/blog/blog-post-preview-list.js
+++ web/src/components/blog/blog-post-preview-list.js
@@ -8,7 +8,7 @@ function BlogPostPreviewGrid(props) {
   return (
     <div className="w-full">
       {props.title && <h2 className={styles.headline}>{props.title}</h2>}
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full grid-rows-2 gap-6 h-auto">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-6">
         {props.nodes &&
           props.nodes.map(node => (
             <li key={node.id}>
