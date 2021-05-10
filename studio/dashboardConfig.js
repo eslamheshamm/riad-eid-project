ab94a3ea98b20eb58ab4129ef61f1e94fba68ad9
eslamheshamm <esl2mhesham@gmail.com>
2021-05-10 02:02:42 +0200

    updating info

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,6 +1,6 @@
 export default {
   widgets: [
-    {name: 'structure-menu'},
+    { name: 'structure-menu' },
     {
       name: 'project-info',
       options: {
@@ -13,35 +13,39 @@ export default {
               sites: [
                 {
                   buildHookId: '603d6989e2d59d2a877b319e',
-                  title: 'Sanity Studio',
-                  name: 'sanity-kitchen-sink-studio-6tgjicjp',
-                  apiId: '67581c06-e629-4c0b-8b93-f499f00e3d08'
+                  title: 'Studio',
+                  name: 'riad-eid-studio',
+                  apiId: '67581c06-e629-4c0b-8b93-f499f00e3d08',
                 },
                 {
                   buildHookId: '603d698ae2d59d2ed97b2f98',
-                  title: 'Blog Website',
-                  name: 'sanity-kitchen-sink-web-fibgjnm6',
-                  apiId: '46e27a25-ef45-483a-aa5d-9f45af39f652'
-                }
-              ]
-            }
-          }
+                  title: 'Riad Eid',
+                  name: 'riad-eid',
+                  apiId: '46e27a25-ef45-483a-aa5d-9f45af39f652',
+                },
+              ],
+            },
+          },
         ],
         data: [
           {
             title: 'GitHub repo',
-            value: 'https://github.com/eslamheshamm/sanity-kitchen-sink',
-            category: 'Code'
+            value: 'https://github.com/eslamheshamm/ried-eid-project',
+            category: 'Code',
           },
-          {title: 'Frontend', value: 'https://sanity-kitchen-sink-web-fibgjnm6.netlify.app', category: 'apps'}
-        ]
-      }
+          {
+            title: 'Frontend',
+            value: 'riadeid.net',
+            category: 'apps',
+          },
+        ],
+      },
     },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: 'project-users', layout: { height: 'auto' } },
     {
       name: 'document-list',
-      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
-      layout: {width: 'medium'}
-    }
-  ]
+      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
+      layout: { width: 'medium' },
+    },
+  ],
 }
