8dcc70c2512841e56c127c704da7c11936b2b063
eslamheshamm <esl2mhesham@gmail.com>
2021-07-03 16:49:14 +0200

    adding formId

--- web/src/pages/contact-us.js
+++ web/src/pages/contact-us.js
@@ -9,12 +9,13 @@ const ContactUs = props => {
   return (
     <Layout>
       <SEO title={"تواصل معنا"} description={"طریقة تحدید جنس المولود قبل الحمل بشكل عملي"} />
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-16 text-white  w-full">
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-16 ite  w-full">
         <FormInputs
           title={"أرسل لنا رسالة"}
           placeHolder1={"عنوان البريد الالكتروني"}
           placeHolder2={`رقم الهاتف`}
           placeHolder3={"الرسالة"}
+          formId="VH57lEMv"
         />
         <img src={Child} alt="Child" className="row-start-1 lg:row-auto" />
       </section>
